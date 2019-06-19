4a28860c82b3c53397e8608885c4a81f6dc63685
HotPot <zhuruilicust@163.com>
2019-06-19 11:53:41 +0800

    python不适合多线程(共享内存空间），设计上有GIL,本质上还是单线程 多进程或线程的出现时由于CPU运算很快，寄存，内存跟不上 多进程更稳定，还可以分布式计算

--- py.checkio/进程线程协程.py
+++ py.checkio/进程线程协程.py
@@ -112,3 +112,82 @@ if __name__=='__main__':
     pw.join()
     # pr进程里是死循环，无法等待其结束，只能强行终止:
     pr.terminate()
+
+#多线程
+#_thread(低级模块），threading,对_thread进行封装
+#启动一个线程就是把一个函数传入并创建Thread实例，然后调用start()开始执行
+import threading
+def loop():
+    print('thread %s is running!'%threading.current_thread().name)
+    n=0
+    while n<5:
+        n+=1
+        print('thread %s >>> %s'%(threading.current_thread().name,n))
+        time.sleep(2)
+    print('thread %s ended'%threading.current_thread().name)
+
+
+print('threading %s is running'%threading.current_thread().name) #任何进程默认就会启动一个线程，我们把该线程称为主线程(MainThread)
+t = threading.Thread(target=loop,name='Loopthread')              #函数实例化
+t.start()                                                        #线程开始
+t.join()
+print('thread %s ended'%threading.current_thread().name)
+#一个问题，线程之间共享数据，会引起影读和脏读，解决方法：上锁
+#局部变量混乱
+balance = 0
+lock = threading.Lock()
+def change_it(n):
+    # 先存后取，结果应该为0:
+    global balance
+    balance = balance + n
+    balance = balance - n
+
+def run_thread(n):
+    for i in range(100000):
+        change_it(n)
+
+
+def run_thread(n):
+    for i in range(100000):
+        # 先要获取锁:
+        lock.acquire()
+        try:
+            # 放心地改吧:
+            change_it(n)
+        finally:
+            # 改完了一定要释放锁:
+            lock.release()
+#问题：实际上是单线程模式，而且可以存在多个锁，不同线程
+#试图获取对方的锁时可能造成死锁，导致既不执行也无法结束，只能考操作系统强制终止
+
+#针对python,按理说，多核多线程是可以实现的
+#但是有GIL锁的存在，多线程只能交替执行
+#任何Python线程执行前，必须先获得GIL锁，然后，每执行100条字节码，解释器就自动释放GIL锁，让别的线程有机会执行。
+# 这个GIL全局锁实际上把所有线程的执行代码都给上了锁
+#所以python环境下，只有用多进程，每个进程有各自独立的GIL锁
+
+
+#这里时针对一个线程的局部变量在调用函数时传递更简便
+# 创建全局ThreadLocal对象:一个线程使用自己的局部变量作函数调用时，传递起来很麻烦
+local_school = threading.local()
+
+
+def process_student():
+    # 获取当前线程关联的student:
+    std = local_school.zzz      #局部变量之间的传递
+    print('Hello, %s (in %s)' % (std, threading.current_thread().name))
+
+
+def process_thread(name):
+    # 绑定ThreadLocal的student:
+    local_school.zzz = name     #对象动态添加属性，同一个线程内
+    process_student()           #调用了process_student 函数
+
+
+t1 = threading.Thread(target=process_thread, args=('Alice',), name='Thread-A')#这是一个线程
+t2 = threading.Thread(target=process_thread, args=('Bob',), name='Thread-B')  #这是另一个线程，每个线程处理的student不一样
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(process_thread('ali'))
