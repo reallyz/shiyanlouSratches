a5a125074e02774d1b2b21c995fa8ba7433fed72
HotPot <zhuruilicust@163.com>
2019-07-07 22:05:32 +0800

    完成一部分，数据的存储还待完善

--- onlinep/playwithnumbers.py
+++ onlinep/playwithnumbers.py
@@ -1,14 +1,35 @@
 
 from math  import modf
-def splitn(n):
+def splitn(n:int,k=2):
     t=n
-    print(t,type(t))
+    times=k*n
+    #print(t,type(t))
     a=[]
+    c=[]
     while t!=0:
-        a.append(int(modf(t/10)[0]*10))
-        print(a)
+        a.append(round(modf(t/10)[0]*10))
+        #print(a)
         b=modf(t/10)[1]
         t=b
-    return a.reverse()
+    a.sort()
+    while times!=0:
+        c.append(round(modf(times / 10)[0] * 10))
+        #print(c)
+        d = modf(times/10)[1]
+        times = d
+    c.sort()
+    if a==c:
+        #return print('Yes!The number is:\n',n*k)
+        return True
+    '''
+    else:
+        return print('No!')
 
-splitn(220)
+    '''
+#find the number under 123456789+1:
+
+for x in range(123456789+1):
+    a=[]
+    if splitn(x):
+        print(x)
+        a.append(x)
---
+++ onlinep/sortarraybyelement.py
@@ -0,0 +1,3 @@
+
+a=[4, 6, 2, 2, 6, 4, 4, 4]
+a.sort(key=lambda x:a.count(x))
