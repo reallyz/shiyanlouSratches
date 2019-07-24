bdab8ba6849daa278897b1c97c209e2daadf05b3
HotPot <zhuruilicust@163.com>
2019-07-24 04:01:25 -0400

    递归：函数加分支，基例（停止条件，链条（此次和上次的关系

--- pymooc/functionuse.py
+++ pymooc/functionuse.py
@@ -22,3 +22,48 @@ def p(o):
     return
 p(10)
 
+#digit tube
+import time
+import turtle
+def drawLine(draw):   #绘制单段数码管
+    turtle.pendown() if draw else turtle.penup()
+    turtle.fd(40)
+    turtle.right(90)
+def drawDigit(digit): #根据数字绘制七段数码管
+    drawLine(True) if digit in [2,3,4,5,6,8,9] else drawLine(False)
+    drawLine(True) if digit in [0,1,3,4,5,6,7,8,9] else drawLine(False)
+    drawLine(True) if digit in [0,2,3,5,6,8,9] else drawLine(False)
+    drawLine(True) if digit in [0,2,6,8] else drawLine(False)
+    turtle.left(90)
+    drawLine(True) if digit in [0,4,5,6,8,9] else drawLine(False)
+    drawLine(True) if digit in [0,2,3,5,6,7,8,9] else drawLine(False)
+    drawLine(True) if digit in [0,1,2,3,4,7,8,9] else drawLine(False)
+    turtle.left(180)
+    turtle.penup()
+    turtle.fd(20)
+def drawDate(date):  #获得要输出的数字
+    w={'+':'日','-':'年','=':'月'}
+    for i in date:
+        if i in w:
+            turtle.write(w[i],font=('Arial',18))
+            turtle.fd(60)
+            turtle.pencolor('blue')
+        else:
+            drawDigit(eval(i))  #通过eval()函数将数字变为整数
+def main():
+    turtle.setup(800, 350, 200, 200)
+    turtle.penup()
+    turtle.fd(-300)
+    turtle.pensize(5)
+    drawDate(time.strftime('%Y-%m=%d+',time.gmtime()))
+    turtle.hideturtle()
+    turtle.done()
+main()
+
+def fact(n):
+    if n==1:
+        return 1
+    else:
+        return n*fact(n-1)
+
+print(fact(5))
