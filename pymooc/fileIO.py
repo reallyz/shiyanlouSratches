54e2dfb0074043c9ba2beed8dd7a835ac89e9c98
HotPot <zhuruilicust@163.com>
2019-07-26 03:50:29 -0400

    字符串的读取

--- pymooc/fileIO.py
+++ pymooc/fileIO.py
@@ -5,5 +5,18 @@ with open('output.txt','a+') as f:
     for line in f:
         print(line)
 
+row=0
+col=0
+with open('/root/桌面/history.txt','r+',encoding='utf-8') as f:
+    for line in f:
+        print(type(line))
+        if line.strip():
+            row+=1
+            for a in line:
+                if a not in "[\\]|,.<>?/:;!\"@#$%^&*()_+-=~`\'\n' '":
+                    col+=1
+    ls=['zhnong\n','guo\n','hao\n']
+    f.writelines(ls)
 
-#自动轨迹绘制,数据类型的把握
+print(row,col)
+#自动轨迹绘制,数据类型的把握,数据和程序的隔离
