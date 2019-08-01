7095e8bcbacc6002b5fca9b5f4ebb36a49c90b17
HotPot <zhuruilicust@163.com>
2019-07-31 21:56:43 -0400

    信息的标记，信息的提取，正则表达式的使用

--- PYvs/l-bs4.py
+++ PYvs/l-bs4.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from sys import path
+import re
 path.append('/root/PycharmProjects/shiyanlouSratches/PYvs')
 import lrequests as lre
 #信息提取
@@ -7,12 +8,25 @@ url='http://python123.io/ws/demo.html'
 r=lre.gethtml(url)
 demo=r.text
 soup=BeautifulSoup(demo,'html.parser')
-print(soup.prettify())
-print(soup.a.prettify())
+print(soup.prettify())#对整体
+print(soup.a.prettify())#对标签
 print(soup.title)
 print(soup.a.parent.name)
 print(soup.a.parent.parent.name)
 print(soup.a.attrs['href'])
-print(soup.a.string)
-#标签树遍历
-#下行
+print(soup.a.get('class'))
+print('this is a type',type(soup.a))
+#标签树遍历,有迭代类型
+#下行，上行，平行
+#信息的标记：html,xml,json,yaml
+#信息的提取
+for link in soup.find_all('a'):
+    print(type(link))
+    print(link.get('href'))
+
+for tag in soup.find_all(re.compile('b')):
+    print(tag.name)
+
+print(soup.find_all(id=re.compile('link')))
+print(soup.find_all(string='Basic Python'))
+print('find python',soup.find_all(string=re.compile('(P|p)ython')))#大小写的问题
