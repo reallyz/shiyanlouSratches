af952e222a370ca4cccdb2b1861c681adf5edca9
HotPot <zhuruilicust@163.com>
2019-08-15 22:59:27 -0400

    re的进一步使用，match方法作过滤很好用

--- PYvs/l-re.py
+++ PYvs/l-re.py
@@ -1,8 +1,8 @@
 import  re
-ls=re.findall(r'[0-9]\d{5}','BIT 100081')
+ls=re.findall(r'[0-9]\d{5}','BIT 100081')#信息提取的时候很好用
 if ls:
     print(ls)
-
+#findall,finditer都是多次匹配，一个输出的是列表，一个输出迭代对象
 for m in re.finditer(r'[1-9]\d{5}','e100081 r100082'):
     if m:
         print(m.group(0))
