2e11ceb8fa1e0d757908370dd60fcc5c7445f00b
HotPot <zhuruilicust@163.com>
2019-10-31 22:54:26 -0400

    pyhton包（模块的搜索路径）的添加 sys.path.append(包所在路经（绝对路径）) 解决红色下划线的问题，文件结构上要作修改（作为source package 就可以）

---
+++ codeveryday/day1/b.py
@@ -0,0 +1,13 @@
+import sys
+
+from a import addnum
+
+sys.path.append('/root/PycharmProjects/shiyanlouSratches/py.checkio/')
+
+
+def main():
+    print(addnum(1,2))
+    print(sys.path)
+    #download('sucks')
+if __name__ == '__main__':
+    main()
