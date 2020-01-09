71a603ab111c7c5f619058fb2b3f0e9e02ff20d6
HotPot <zhuruilicust@163.com>
2020-01-09 16:43:02 +0800

    1.join方法：阻塞主进程，直到子进程执行完毕 2.python是整体编译的

---
+++ py.checkio/进程线程(二）.py
@@ -0,0 +1,26 @@
+﻿import os
+from multiprocessing import Process
+import time
+def p_1(name):
+    time.sleep(2)
+    print('%s is running %s '%(name,os.getpid()))
+def hello_world():
+    time.sleep(3)
+    print('%s is running and my father is %s'%(os.getpid(),os.getppid()))
+
+if __name__=='__main__':
+    print('Parent process number is %s'%(os.getpid()))
+    p1=Process(target=p_1,args=('test',))
+    p2=Process(target=hello_world)
+    '''
+    #不指定target什么都不会执行(python文档)
+    p1=Process()
+    p1.run=p_1 #手动重写run方法
+    p1.start() #启动进程
+    '''
+    p1.start()
+    p2.start()
+    p1.join()  #join会阻塞主进程，直到子进程执行完毕。同时这也证明了python不是解释一句执行一句，而是作为整体编译后再执行
+              #不然，join就不会起作用了
+    p2.join()
+    print('Process ends')
