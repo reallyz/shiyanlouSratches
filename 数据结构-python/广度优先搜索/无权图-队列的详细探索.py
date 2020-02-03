e5d8532b77685c015ed4b5953368cb7b259b26a0
HotPot <zhuruilicust@163.com>
2020-02-03 16:56:00 +0800

    python标准库queue,实现队列，栈，优先级队列

---
+++ 数据结构-python/广度优先搜索/无权图-队列的详细探索.py
@@ -0,0 +1,47 @@
+﻿
+# 二维数据存储定点的关系
+# 队列存储顶点
+#
+import  queue,random
+n=5
+q=queue.Queue(n)
+print('入队')
+for i in range(n):
+    d=random.randint(0,100)
+    print(d)
+    q.put(d)
+print('出队')
+while q.empty()==False:
+    print(q.get())
+# 在python中，queue标准库不仅能实现队列(FIFO)
+# 还能实现栈(LIFO)
+# 还能实现优先级队列
+
+# 用queue实现队列
+print('queue实现栈')
+def TestStack(n):
+    s=queue.LifoQueue(n)
+    print('入栈')
+    for i in range(n):
+        d=random.randint(0,100)
+        s.put(d)
+        print(d)
+    print('出栈')
+    while s.empty()==False:
+        print(s.get())
+
+TestStack(n)
+
+# 优先级队列的实现
+def PriorityQueue(n):
+    '对元素值越小，优先级越高，越先出'
+    p=queue.PriorityQueue(n)
+    for i in range(n):
+        d=random.randint(0,100)
+        p.put(d)
+        print(d)
+    print('优先级队列出队')
+    while p.empty()==False:
+        print(p.get())
+
+PriorityQueue(n)
