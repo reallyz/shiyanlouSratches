37e52c9df7dc44e1b1ae5191c5244740cc0d2889
HotPot <zhuruilicust@163.com>
2020-02-08 15:30:57 +0800

    模块和函数的调用

---
+++ pythoncookbook基础版/fibo.py
@@ -0,0 +1,18 @@
+﻿def  fib(n):
+    # print fib up to n
+    a,b=0,1
+    while b<n:
+        print(b,end=' ')
+        a,b=b,a+b
+    print()
+
+
+def fib2(n):
+    a,b=0,1
+    ls=[]
+    while b<n:
+        ls.append(b)
+        a,b=b,a+b
+    return  ls
+
+vinm=10
